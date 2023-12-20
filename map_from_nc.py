c284199855fdbf11e542d4e6e041a4ee7c515f68
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2023-12-20 09:46:30 +0100

    added more examples

--- map_from_nc.py
+++ map_from_nc.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import rioxarray as rio
 
-# writing direct to tiff does not work
+# write direct to geotiff
 try:
     nc = xr.open_dataset('./output/geotop.nc')
     print(nc)
@@ -9,8 +9,27 @@ try:
 except Exception as e:
     print(e)
 
-# making a new DataArray does not work either
+# make a new DataArray
 try:
+    data = nc['lithok'].data
+    xrd = nc['xrd'].data
+    yrd = nc['yrd'].data
+    z = nc['z'].data
+    xray = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
+    nc.rio.to_raster(r'output.tiff')
+except Exception as e:
+    print(e)
+
+# transpose
+try:
+    nc = nc.transpose()
+    nc.rio.to_raster(r'output.tiff')
+except Exception as e:
+    print(e)
+
+# transpose and make a new DataArray
+try:
+    nc = nc.transpose()
     data = nc['lithok'].data
     xrd = nc['xrd'].data
     yrd = nc['yrd'].data
