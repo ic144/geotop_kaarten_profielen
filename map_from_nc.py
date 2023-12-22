e6297925b61b09d1fee1f9a1f879a1c97b7cfc67
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2023-12-22 11:12:07 +0100

    fixed error and added another try

--- map_from_nc.py
+++ map_from_nc.py
@@ -1,22 +1,35 @@
 import xarray as xr
 import rioxarray as rio
+import matplotlib.pyplot as plt
+
 
 # write direct to geotiff
 try:
     nc = xr.open_dataset('./output/geotop.nc')
-    print(nc)
     nc.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
 
-# make a new DataArray
+# make a new DataArray with all data
 try:
     data = nc['lithok'].data
     xrd = nc['xrd'].data
     yrd = nc['yrd'].data
     z = nc['z'].data
     xray = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
-    nc.rio.to_raster(r'output.tiff')
+    xray.rio.to_raster(r'output.tiff')
+except Exception as e:
+    print(e)
+
+# make a new DataArray with one layer
+try:
+    nc = xr.open_dataset('./output/geotop.nc')
+    data = nc['lithok'].data[0]
+    xrd = nc['xrd'].data
+    yrd = nc['yrd'].data
+    z = nc['z'].data
+    xray = xr.DataArray(data, dims=('yrd', 'xrd'), coords={'xrd': xrd, 'yrd': yrd})
+    xray.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
 
@@ -38,3 +51,4 @@ try:
     nc.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
+
