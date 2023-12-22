f766ab2437c440471f71fcc23eea167a78719f7a
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2023-12-22 11:14:54 +0100

    fixed errors

--- map_from_nc.py
+++ map_from_nc.py
@@ -16,39 +16,38 @@ try:
     xrd = nc['xrd'].data
     yrd = nc['yrd'].data
     z = nc['z'].data
-    xray = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
-    xray.rio.to_raster(r'output.tiff')
+    da = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
+    da.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
 
 # make a new DataArray with one layer
 try:
-    nc = xr.open_dataset('./output/geotop.nc')
     data = nc['lithok'].data[0]
     xrd = nc['xrd'].data
     yrd = nc['yrd'].data
     z = nc['z'].data
-    xray = xr.DataArray(data, dims=('yrd', 'xrd'), coords={'xrd': xrd, 'yrd': yrd})
-    xray.rio.to_raster(r'output.tiff')
+    da = xr.DataArray(data, dims=('yrd', 'xrd'), coords={'xrd': xrd, 'yrd': yrd})
+    da.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
 
 # transpose
 try:
-    nc = nc.transpose()
-    nc.rio.to_raster(r'output.tiff')
+    ncT = nc.transpose()
+    ncT.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
 
 # transpose and make a new DataArray
 try:
-    nc = nc.transpose()
-    data = nc['lithok'].data
-    xrd = nc['xrd'].data
-    yrd = nc['yrd'].data
-    z = nc['z'].data
-    xray = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
-    nc.rio.to_raster(r'output.tiff')
+    ncT = nc.transpose()
+    data = ncT['lithok'].data
+    xrd = ncT['xrd'].data
+    yrd = ncT['yrd'].data
+    z = ncT['z'].data
+    da = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
+    da.rio.to_raster(r'output.tiff')
 except Exception as e:
     print(e)
 
