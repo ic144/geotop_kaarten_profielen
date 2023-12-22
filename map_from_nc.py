8e013ce8b72647afa1cb90a8d2e361d1bf0e3cfc
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2023-12-22 11:24:59 +0100

    added save as netcdf

--- map_from_nc.py
+++ map_from_nc.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 # write direct to geotiff
 try:
     nc = xr.open_dataset('./output/geotop.nc')
-    nc.rio.to_raster(r'output.tiff')
+    nc.to_netcdf('output1.nc')
+    nc.rio.to_raster(r'output1.tiff')
 except Exception as e:
     print(e)
 
@@ -17,7 +18,8 @@ try:
     yrd = nc['yrd'].data
     z = nc['z'].data
     da = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
-    da.rio.to_raster(r'output.tiff')
+    da.to_netcdf('output2.nc')
+    da.rio.to_raster('output2.tiff')
 except Exception as e:
     print(e)
 
@@ -28,14 +30,16 @@ try:
     yrd = nc['yrd'].data
     z = nc['z'].data
     da = xr.DataArray(data, dims=('yrd', 'xrd'), coords={'xrd': xrd, 'yrd': yrd})
-    da.rio.to_raster(r'output.tiff')
+    da.to_netcdf('output3.nc')
+    da.rio.to_raster('output3.tiff')
 except Exception as e:
     print(e)
 
 # transpose
 try:
     ncT = nc.transpose()
-    ncT.rio.to_raster(r'output.tiff')
+    ncT.to_netcdf('output4.nc')
+    ncT.rio.to_raster('output4.tiff')
 except Exception as e:
     print(e)
 
@@ -47,7 +51,8 @@ try:
     yrd = ncT['yrd'].data
     z = ncT['z'].data
     da = xr.DataArray(data, dims=('z', 'y', 'x'), coords={'x': xrd, 'y': yrd})
-    da.rio.to_raster(r'output.tiff')
+    da.to_netcdf('output5.nc')
+    da.rio.to_raster('output5.tiff')
 except Exception as e:
     print(e)
 
